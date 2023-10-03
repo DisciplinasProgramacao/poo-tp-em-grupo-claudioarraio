e06c6f294e6f1b442c55082962028e6c3dbac7f1
Carlos Corvaum <101909778+carlosCorvaum@users.noreply.github.com>
2023-10-03 15:58:29 -0300

    V0.9
    TO DO:
    
    -ToString de Rotas
    -Code Reveal para deixar o codigo mais limpo
    -Finalização do APP.
    
    Co-Authored-By: Daniel Almeida Lopes <116038774+Danlopesz@users.noreply.github.com>
    Co-Authored-By: victortvc <116039551+victortvc@users.noreply.github.com>
    Co-Authored-By: Victor <victorg17@live.com>

--- codigo/src/Rota.java
+++ codigo/src/Rota.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import java.util.Date;
 
 public class Rota {
@@ -19,3 +20,30 @@ public class Rota {
     }
 
 }
+=======
+import java.util.Date;
+
+public class Rota {
+    private double quilometragem;
+    private Date data;
+
+    public Date getData() {
+        return data;
+    }
+
+    public double getQuilometragem() {
+        return quilometragem;
+    }
+
+    public Rota(double quilometragem, Date data) {
+        this.quilometragem = quilometragem;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() { // Relatorio
+        return "0.2";
+    }
+
+}
+>>>>>>> Stashed changes
