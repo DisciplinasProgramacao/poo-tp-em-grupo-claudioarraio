4239b9ffefc5c9465f80aa65d28ae34ba2ac9767
Carlos Corvaum <101909778+carlosCorvaum@users.noreply.github.com>
2023-10-03 16:23:45 -0300

    V0.9
    TO DO:
    
    -ToString de Rotas
    -Code Reveal para deixar o codigo mais limpo
    -Finalização do APP.
    
    Co-Authored-By: Daniel Almeida Lopes <116038774+Danlopesz@users.noreply.github.com>
    Co-Authored-By: victortvc <116039551+victortvc@users.noreply.github.com>
    Co-Authored-By: Victor <victorg17@live.com>

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -1,5 +1,57 @@
+<<<<<<< Updated upstream
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
+=======
+import java.time.*;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Crie um objeto de frota
+        Frota frota = new Frota();
+
+        // Crie alguns veículos
+        Tanque tanque1 = new Tanque(50.0, 60.0);
+        Veiculo veiculo1 = new Veiculo("ABC123", 0, tanque1, 0.0);
+
+        Tanque tanque2 = new Tanque(50.0, 60.0);
+        Veiculo veiculo2 = new Veiculo("BC123", 0, tanque1, 0.0);
+
+        // Registre algumas rotas
+        Rota rota1 = new Rota(100.0, new Date());
+        Rota rota2 = new Rota(110.0, new Date());
+        // Criando outras rotas
+        Rota rota3 = new Rota(10.0, new Date());
+        Rota rota4 = new Rota(110.0, new Date());
+
+        // Adicioanr Rota no respectivo veiculo
+        veiculo1.addRota(rota1);
+        veiculo1.addRota(rota2);
+
+        // Adicionando Rota no respectivo veiculo
+        veiculo2.addRota(rota3);
+        veiculo2.addRota(rota4);
+
+        // Adicione os veículos à frota
+        frota.addVeiculos(veiculo1);
+        frota.addVeiculos(veiculo2);
+
+        // System.out.println(frota.maiorKmMedia().getPlaca());
+
+        // System.out.println(frota.maiorKmTotal().getPlaca());
+
+        // Adicionar veiculo (param = placa);
+        // Criar rota (param= quilometragem e data)
+        // Adicionar rota em determinado veiculo (void ou boolea)
+        // localizar veiculo e imprimir seus respectivos dados (retorna String)
+
+        System.out.println(frota);
+
+    }
+
+}
+>>>>>>> Stashed changes
--- codigo/src/Veiculo.java
+++ codigo/src/Veiculo.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import java.util.*;
 
 public class Veiculo {
@@ -67,3 +68,109 @@ public class Veiculo {
     }
 
 }
+=======
+import java.util.*;
+
+public class Veiculo {
+    private static int MAX_ROTAS = 30;
+    private static double CONSUMO = 8.2;
+    private String placa;
+    private int quantRotas=0;
+    private ArrayList<Rota> rotas = new ArrayList<Rota>();
+    private Tanque tanque;
+    private Double totalReabastecido;
+
+
+    
+    public Double getTotalReabastecido() {
+        return totalReabastecido;
+    }
+
+    public Veiculo(String placa, int quantRotas, Tanque tanque, Double totalReabastecido) {
+        this.placa = placa;
+        this.quantRotas = quantRotas;
+        this.tanque = tanque;
+        this.totalReabastecido = totalReabastecido;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+        public Tanque getTanque() {
+        return tanque;
+    }
+
+    public boolean addRota(Rota rota) {
+
+        if (autonomiaAtual() >= rota.getQuilometragem()) {
+            rotas.add(rota);
+            return true;
+        }
+        return false;
+    }
+
+    private Double autonomiaMaxima() {
+        return tanque.autonomiaMaxima();
+    }
+
+    private Double autonomiaAtual() {
+
+        return this.tanque.autonomiaAtual();
+
+    }
+
+    public Double abastecer(Double litros) {
+
+        this.totalReabastecido+=litros;
+        return tanque.abastecer(litros);
+
+    }
+
+    public Double kmNoMes() {
+        Calendar calAtual = Calendar.getInstance();
+        Double quilometragemMes = 0.0;
+
+        for (Rota rota : rotas) {
+            Calendar calRota = Calendar.getInstance();
+            calRota.setTime(rota.getData());
+
+            if (calAtual.get(Calendar.MONTH) == calRota.get(Calendar.MONTH)) {
+                quilometragemMes += rota.getQuilometragem();
+            }
+        }
+
+        return quilometragemMes;   
+    }
+
+    public Double kmTotal() {
+        Double quilometragemTotal = 0.0;
+
+        for (Rota rota : rotas) {
+            quilometragemTotal += rota.getQuilometragem();
+        }
+
+        return quilometragemTotal;
+    }
+     public int getQuantRotas() {
+        return quantRotas;
+    }
+    
+
+    private void percorrerRota(Rota rota) {
+
+        
+        Double distPercorrida = rota.getQuilometragem();
+
+    }
+
+}
+
+
+//Placa:
+//Litros abastecidos:
+//Quantidade de rotas:
+//KM no mes:
+//KM total:
+//Km media:
+
+>>>>>>> Stashed changes
