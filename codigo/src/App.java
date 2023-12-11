e868398832536734cab76ef066875bee2700e367
Victor <victorg17@live.com>
2023-12-11 16:40:48 -0300

    menu atualizado

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -6,72 +6,43 @@ public class App {
 
     static Scanner sc = new Scanner(System.in);
     public static String placa;
-//#region criando comandos de limpeza e pausa
+
+    // #region criando comandos de limpeza e pausa
     static void pausa() {
         System.out.println("Enter para continuar.");
         sc.nextLine();
     }
+
     public static void limparTela() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-//#endregion
+    // #endregion
 
+    // #region criação da abertura de App e menus para que o usuário escolha através
+    // do Scanner(teclado)
 
-//#region criação da abertura de App e menus para que o usuário escolha através do Scanner(teclado)  
- 
     public static void abrirApp() {
         System.out.println("APP FROTAS");
-        System.out.println("=========");
+        System.out.println("\n ======================\n");
 
         System.out.println("1 - Gerenciar veículos");
         System.out.println("2 - Gerar relatórios");
         System.out.println("0 - Sair");
+        System.out.println();
+        System.out.println("Insira uma opção: ");
 
         int num = sc.nextInt();
 
         switch (num) {
             case 1:
                 menuVeiculo(num, null);
-            break;
+                break;
             case 2:
                 menuRelatorio(num);
-            break;
-            case 0: 
                 break;
-        }
-    }
-
-    public static void menuVeiculo(int num, Rota rota) {
-        System.out.println("1 - Cadastrar veículo");
-        System.out.println("2 - Cadastrar rota");
-        System.out.println("3 - Abaster veículo");
-        System.out.println("4 - Localizar veículo");
-        System.out.println("5 - Percorrer rota");
-
-        num = sc.nextInt();
-
-        switch (num) {
-            case 1:
-                // Veiculo veiculo1 = new Veiculo("ABC123", 0, tanque);
-                break;
-            case 2:
-                //
-                break;            
-            case 3:
-                // veiculo.abastecer
-                break;
-            case 4:
-                System.out.println("Insira a placa do veículo: ");
-                placa = sc.nextLine();
-                //localizarVeiculo(placa);
-                System.out.println("SWITCH 4 TESTADO");
-                break;
-            case 5:
-                System.out.println("Insira a placa do veículo que irá percorrer a rota: ");
-                placa = sc.nextLine();
-                //Veiculo.percorrerRota(rota);
-                System.out.println("SWITCH 5 TESTADO");
+            case 0:
+                System.out.println("APP Finalizado");
                 break;
             default:
                 System.out.println("Valor inválido");
@@ -79,38 +50,90 @@ public class App {
         }
     }
 
-    public static void menuRelatorio(int num) {
-        limparTela();
+    public static void menuVeiculo(int num, Rota rota) {
+        System.out.println("\n MENU DE VEÍCULOS");
+        System.out.println("\n ======================\n");
 
-        System.out.println("1 - Relatório de despesas");
-        System.out.println("2 - Relatório de um veículo individual");
-        System.out.println("3 - Relatório da frota");
+        System.out.println("1 - Cadastrar veículo");
+        System.out.println("2 - Cadastrar rota");
+        System.out.println("3 - Abaster veículo");
+        System.out.println("4 - Localizar veículo");
+        System.out.println("5 - Percorrer rota");
+        System.out.println("0 - Voltar ao menu principal");
 
         num = sc.nextInt();
 
         switch (num) {
             case 1:
-                //fazer relatorio despesas
+                // Veiculo veiculo1 = new Veiculo("ABC123", 0, tanque);
+                System.out.println("SWITCH 1");
                 break;
             case 2:
-                //veiculo.
+                //
+                System.out.println("SWITCH 2");
                 break;
+            case 3:
+                // veiculo.abastecer
+                System.out.println("SWITCH 3");
+                break;
+            case 4:
+                System.out.println("Insira a placa do veículo: ");
+                placa = sc.nextLine();
+                // localizarVeiculo(placa);
+                System.out.println("SWITCH 4");
+                break;
+            case 5:
+                System.out.println("Insira a placa do veículo que irá percorrer a rota: ");
+                placa = sc.nextLine();
+                // Veiculo.percorrerRota(rota);
+                System.out.println("SWITCH 5");
+                break;
+            case 0:
+                limparTela();
+                abrirApp();
             default:
-            System.out.println("Valor inválido");
+                System.out.println("Valor inválido");
                 break;
         }
     }
-    //#endregion
+
+    public static void menuRelatorio(int num) {
+        System.out.println("\n MENU DE RELATÓRIOS");
+        System.out.println("\n ======================\n");
+
+        System.out.println("1 - Relatório de despesas");
+        System.out.println("2 - Relatório de um veículo individual");
+        System.out.println("3 - Relatório da frota");
+        System.out.println("0 - Voltar ao menu principal");
+        num = sc.nextInt();
+
+        switch (num) {
+            case 1:
+                // fazer relatorio despesas
+                break;
+            case 2:
+                // veiculo.
+                break;
+            case 3:
+                // relatorio toString frota
+            case 0:
+                limparTela();
+                abrirApp();
+            default:
+                System.out.println("Valor inválido");
+                break;
+        }
+    }
+    // #endregion
 
     public static void main(String[] args) throws Exception {
 
         abrirApp();
-        limparTela();
 
         // menuVeiculo(sc.nextInt());
 
         // Crie um objeto de frota
-        //Frota frota = new Frota();
+        // Frota frota = new Frota();
 
         // Crie alguns veículos
         // Tanque tanque1 = new Tanque(50.0, 60.0);
